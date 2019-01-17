79025458743252698cbe7e473966e510a8576b6d
Mohoshina Akter <mohoshina.akter@ntdac.naztech.local>
2019-01-17 13:55:34 +0600

    first commit

---
+++ src/main/java/Timeandday/Timeandday/Converter.java
@@ -0,0 +1,99 @@
+package Timeandday.Timeandday;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class Converter {
+
+	public static LocalDate datetolocaldate(Date d) throws ParseException {
+
+		Instant instant = d.toInstant();
+		ZonedDateTime date1 = instant.atZone(ZoneId.systemDefault());
+		System.out.println("localTime"+date1);
+		DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-LLLL-dd");
+		String formattedString1 = date1.format(formatter1);
+		String x = formattedString1.substring(0, 5) + "0" + formattedString1.substring(5, formattedString1.length());
+		LocalDate localDate = LocalDate.parse(x);
+		return localDate;
+
+	}
+
+	public static LocalTime dateToLocalTime(Date d) {
+		
+
+	    Instant instant = Instant.ofEpochMilli(d.getTime());
+	    LocalTime res = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalTime();
+	   // System.out.println(res);
+		return res;
+	}
+	
+
+
+public static Date localDatetodate(LocalDate dateToConvert) {
+		
+	return java.util.Date.from(dateToConvert.atStartOfDay()
+		      .atZone(ZoneId.systemDefault())
+		      .toInstant());
+
+	 
+	}
+
+public static Calendar LocalDatetocalender(LocalDate date) {
+	
+    GregorianCalendar gc = GregorianCalendar.from(date.atStartOfDay(ZoneId.systemDefault()));
+	
+	return gc;
+}
+
+public static LocalDate calenderTolocalDate(Object set) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static ZonedDateTime convertCalendarToZonedTime(Calendar cal,ZoneId zoneid)
+{
+	//ZoneId zoneid = ZoneId.of("Asia/Tokyo");
+	
+	
+	ZonedDateTime zonedDateTime = ZonedDateTime.of(CalendarToLocalDateTime(cal), zoneid);
+	
+	return zonedDateTime;
+	
+}
+
+
+private static LocalDateTime CalendarToLocalDateTime(Calendar cal) {
+	Date date = cal.getTime();
+	return date.toInstant()
+		      .atZone(ZoneId.systemDefault())
+		      .toLocalDateTime();
+	
+}
+
+
+
+public static OffsetDateTime DateToOffsetDateTime(Calendar cal,ZoneOffset offset)
+{
+
+    OffsetDateTime offsetDateTime = OffsetDateTime.of(CalendarToLocalDateTime(cal), offset);
+    
+    return offsetDateTime;
+}
+
+
+}
---
+++ src/test/java/Timeandday/Timeandday/ConvertorTest.java
@@ -0,0 +1,139 @@
+package Timeandday.Timeandday;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+
+
+
+
+public class ConvertorTest {
+
+
+	public final void datetolocaldate() throws ParseException {
+
+		
+		String sDate5 = "Wed, Jan 16 2019 23:37:50";
+		SimpleDateFormat formatter5 = new SimpleDateFormat("E, MMM dd yyyy HH:mm:ss");
+		Date date5 = formatter5.parse(sDate5);
+		LocalDate d = Converter.datetolocaldate(date5);
+
+		String sDate1 = "2019-01-16";
+		LocalDate date = LocalDate.parse(sDate1);
+
+		assertEquals(date, d);
+	}
+
+	@Test
+	public final void dateToLocalTime() throws ParseException {
+	
+		Converter c = new Converter();
+		String sDate5 = "Wed, Jan 16 2019 15:55:41";
+		SimpleDateFormat formatter5 = new SimpleDateFormat("E, MMM dd yyyy HH:mm:ss");
+		Date date5 = formatter5.parse(sDate5);
+		LocalTime d = c.dateToLocalTime(date5);
+		
+		
+
+		String Time="15:55:41";
+		LocalTime localTime = LocalTime.parse(Time);
+		//System.out.print("localTime"+localTime);
+		
+		assertEquals(localTime, d);
+		
+		
+
+	}
+	
+	@Test
+	public final void LocalDatetocalender() throws ParseException {
+	           
+		String sDate1 = "2019-01-16";
+		LocalDate date = LocalDate.parse(sDate1);
+		Calendar d=Converter.LocalDatetocalender(date);
+		Date dd=d.getTime();
+      TimeZone ddd= d.getTimeZone();
+      System.out.print("localTime"+ddd);
+		String sDate5 = "Wed Jan 16 00:00:00 BDT 2019";
+		SimpleDateFormat formatter5 = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy");
+		Date date5 = formatter5.parse(sDate5);
+	
+	
+		assertEquals(dd, date5 );
+		
+
+		
+
+	}	
+	
+	@Test
+	public final void  localDatetodate() throws ParseException {
+	
+
+		String sDate1 = "2019-01-16";
+		LocalDate date = LocalDate.parse(sDate1);
+		Date d=Converter.localDatetodate(date);
+	
+	
+		
+		String sDate5 = "Wed, Jan 16 2019 00:00:00 BDT 2019";
+		SimpleDateFormat formatter5 = new SimpleDateFormat("E, MMM dd yyyy HH:mm:ss Z yyyy");
+		Date date5 = formatter5.parse(sDate5);
+		
+		
+		
+		assertEquals(date5, d);
+		
+
+	}
+	
+
+	@Test
+	public final void calenderTolocalDate() throws ParseException {
+	
+		Calendar cal = Calendar.getInstance();
+		
+		//LocalDate d= Converter.calenderTolocalDate(cal.set(2019, 01, 16));
+		//System.out.print("Calendar"+ cal);
+		//LocalDateTime l= Converter.calenderTolocalDate(cal);
+	//	System.out.print("localTime"+l);
+		
+		
+
+	}
+	
+	@Test
+	public void CalendarToZonedTime() {
+		ZoneId zoneid = ZoneId.of("Asia/Tokyo");
+		Calendar cal = Calendar.getInstance();
+		ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.now(), zoneid);
+		assertEquals(zonedDateTime.getHour(), Converter.convertCalendarToZonedTime(cal, zoneid).getHour());
+		assertEquals(zonedDateTime.getMinute(), Converter.convertCalendarToZonedTime(cal, zoneid).getMinute());
+	}
+
+	@Test
+	public void CalendarToLocalDateTime() {
+		Calendar cal = Calendar.getInstance();
+		LocalDateTime d1 = LocalDateTime.now();
+		
+	}
+	@Test
+	public void DateToOffsetDateTime() {
+		Calendar cal = Calendar.getInstance();
+		ZoneOffset offset = ZoneOffset.ofHoursMinutes(1, 30);
+		OffsetDateTime dateTime1=OffsetDateTime.of(LocalDateTime.of(2019, 01, 12, 05, 45), ZoneOffset.ofHoursMinutes(6, 30));
+		assertNotEquals(dateTime1,Converter.DateToOffsetDateTime(cal, offset));
+		
+	}
+
+
+
+}
